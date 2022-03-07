67c9d5e0080fb962bf85663069e072c955c37f38
jagan <43448819+jagannath-swarnkar@users.noreply.github.com>
2022-03-07 16:54:56 +0530

    Add files via upload

---
+++ algos.py
@@ -0,0 +1,32 @@
+arr = [23,2,32,57,84,23,64,67,345,23,54,66,23,45,66,34,2,4,1,5,63,4,2,4,4,6,7]
+
+print(arr)
+
+# 1. remove duplicate items from this array
+# 2. find biggest element
+# 3. find smallest element
+# 4. arrange all elements in ascending order
+# 5. arrange all elements in descending order
+# 6. print last element 
+# 7. print second last element of the array
+# 8. reverse this array
+# 9. remove all elemnt "4" from the array
+# 10. print all elements which are divisible by 3
+
+# 11. Write a Python Program to Print Table of a Given Number?
+# 12. How to convert a list into a string?
+
+# # weekdays = ['sun','mon','tue','wed','thu','fri','sun','mon','mon']
+# 13. How to count the occurrences of a particular element in the list? 
+# # for example count number of 'sun' in weekdays array
+
+# 14. How to generate random numbers in Python?
+# 15. Can you write a program to find the average of numbers in a list in Python?
+# Write a program to reverse a number in Python?
+#  # ex-
+# Enter number: 143
+# The reverse of the number: 341
+
+# 16. 
+
+
